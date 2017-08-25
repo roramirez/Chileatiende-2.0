31a88a55549d912f380a5d2b8eb837ffe69adde1
nsilva <nsilva@host28.colocolo.telsur.cl>
2017-08-25 14:45:30 -0300

    Comando para creacion de indices elasticsearch

---
+++ app/Console/Commands/IndexElasticsearch.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Console\Command;
+
+class IndexElasticsearch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'elasticsearch:index {operation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Crea los indices en elasticsearch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $client = new Client();
+
+        $operation = $this->argument('operation');
+
+        if($operation == 'create'){
+            try {
+                $this->info('Se borra índice');
+                $response = $client->request('DELETE', env('ELASTICSEARCH_HOST').':9200/'.env('ELASTICSEARCH_INDEX'));
+            }catch(ClientException $e){
+                $this->line($e->getMessage());
+            }
+
+
+            $this->info('Se crea índice');
+            $response = $client->request('PUT', env('ELASTICSEARCH_HOST').':9200/'.env('ELASTICSEARCH_INDEX'), [
+                'json' => [
+                    'mappings' => [
+                        'pages' => [
+                            'properties' => [
+                                'title' => [
+                                    'type' => 'text',
+                                    'analyzer' => 'spanish'
+                                ],
+                                'objective' => [
+                                    'type' => 'text',
+                                    'analyzer' => 'spanish'
+                                ],
+                                'hit_count' => [
+                                    'type' => 'integer'
+                                ]
+                            ]
+                        ],
+                        'suggestions' => [
+                            'properties' => [
+                                'query' => [
+                                    'type' => 'completion'
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+        }elseif ($operation == 'index'){
+            $this->call('scout:import', ['model' => 'App\Page']);
+            $this->call('scout:import', ['model' => 'App\Suggestion']);
+        }
+
+
+
+
+    }
+}
