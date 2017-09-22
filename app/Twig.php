5299240f41f29a0a25c332bad846c5779476ea0e
Nicolas Silva <nsilvah@gmail.com>
2017-09-22 11:54:08 -0300

    Fix para que no se caiga completo la pagina al existir errores en twig

--- app/Twig.php
+++ app/Twig.php
@@ -6,6 +6,12 @@ namespace App;
 class Twig{
 
     public static function render($string, $data = []){
-        return \Twig::createTemplate($string)->render($data);
+        try{
+            $response = \Twig::createTemplate($string)->render($data);
+        }catch(\Exception $e){
+            return '<p>'.$e->getMessage().'</p>';
+        }
+
+        return $response;
     }
 }
