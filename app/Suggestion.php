f3983b89c4b321c11110887136ab6c9dc3dd400d
Nicolas Silva <nsilvah@gmail.com>
2017-09-29 15:16:33 -0300

    fix en calculo de suggest

--- app/Suggestion.php
+++ app/Suggestion.php
@@ -15,7 +15,7 @@ class Suggestion extends Model
 
         $searches = Search::selectRaw('LOWER(query) as query, COUNT(*) as count')
             ->groupBy('query')
-            ->where('updated_at', '>=', '2017-07-21')
+            ->where('updated_at', '>=', Carbon::now()->subYear())
             ->having('count','>=',100)
             ->get();
 
