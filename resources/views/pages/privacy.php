a62d4d1eed6fed2bfb4f97f29eaae6bb856ce61d
unluck <diego@asimov.cl>
2017-10-17 15:00:23 -0300

    fix page privacidad

--- resources/views/pages/privacy.php
+++ resources/views/pages/privacy.php
@@ -18,33 +18,33 @@
 				<h2>I. Sobre los datos recopilados</h2>
 				<p>MINSEGPRES recopila datos de los suscriptores, usuarios y/o visitantes que hagan uso de este portal, a través de dos mecanismos:</p>
 				<ul>
-					<li>
-						<p><span class="item">a.</span><span>MECANISMOS AUTOMÁTICOS:</span> Son aquellos procesos informáticos realizados para generar registros de las actividades de los visitantes de sitios Web y cuyo objeto es establecer patrones de actividad, navegación y audiencia, que no implican la identificación personal de aquellos suscriptores, usuarios y/o visitantes que accedan al PORTAL CHILEATIENDE.</p>
+					<li class="type-latin">
+						<p><span>MECANISMOS AUTOMÁTICOS:</span> Son aquellos procesos informáticos realizados para generar registros de las actividades de los visitantes de sitios Web y cuyo objeto es establecer patrones de actividad, navegación y audiencia, que no implican la identificación personal de aquellos suscriptores, usuarios y/o visitantes que accedan al PORTAL CHILEATIENDE.</p>
 						<p>MINSEGPRES se reserva el derecho de usar dicha información general, a fin de establecer criterios que mejoren los contenidos de este sistema, en todo caso siempre disociados de la persona que dejó los datos en su navegación.</p>
 					</li>
-					<li>
-						<p><span class="item">b.</span><span>MECANISMOS MANUALES:</span> Son requerimientos formales y expresos de información a los suscriptores, usuarios y/o visitantes del portal que implican la recolección de datos personales tales como nombre, apellidos, domicilio, correo electrónico, ocupación, etc.</li>
+					<li class="type-latin">
+						<p><span>MECANISMOS MANUALES:</span> Son requerimientos formales y expresos de información a los suscriptores, usuarios y/o visitantes del portal que implican la recolección de datos personales tales como nombre, apellidos, domicilio, correo electrónico, ocupación, etc.</li>
 
 						<p>MINSEGPRES recopila estos datos con el objeto de mejorar la calidad de información del portal y dar una mejor atención ante consultas de la ciudadanía.</p>
 					</li>
 				</ul>
 				<hr>
 				<h2>II. Sobre el tratamiento de los datos</h2>
-				<ul type="1">
+				<ul>
 					<li>
-						<span class="item">1.</span><p>Respecto de la entrega de información recopilada por medio de los mecanismos automáticos antes señalados, y que no contengan identificación personal de los suscriptores, usuarios y/o visitantes de la página, esta podrá ser utilizada para informar a entidades públicas, gubernamentales o a terceros sobre patrones de actividad, navegación, audiencia y caracterización general de este sistema.</p>
+						<p>Respecto de la entrega de información recopilada por medio de los mecanismos automáticos antes señalados, y que no contengan identificación personal de los suscriptores, usuarios y/o visitantes de la página, esta podrá ser utilizada para informar a entidades públicas, gubernamentales o a terceros sobre patrones de actividad, navegación, audiencia y caracterización general de este sistema.</p>
 					</li>
 					<li>
-						<span class="item">2.</span><p>Respecto de los datos personales de los usuarios, recolectados a través de mecanismos manuales u otros medios, éstos serán tratados con objeto de mejorar la calidad de información del portal y dar una mejor atención ante consultas de la ciudadanía.</p>
+						<p>Respecto de los datos personales de los usuarios, recolectados a través de mecanismos manuales u otros medios, éstos serán tratados con objeto de mejorar la calidad de información del portal y dar una mejor atención ante consultas de la ciudadanía.</p>
 					</li>
 				</ul>
 				<p>Al aceptar las presentes Políticas de Privacidad, usted autoriza al MINSEGPRES para:</p>
 				<ul>
-					<li>
-						<span class="item">a.</span><p>Comunicar a otros organismos del Estado u otros terceros sus datos personales con el único objeto de mejorar la calidad de información del portal y dar una mejor atención ante consultas de la ciudadanía.</p>
+					<li class="type-latin">
+						<p>Comunicar a otros organismos del Estado u otros terceros sus datos personales con el único objeto de mejorar la calidad de información del portal y dar una mejor atención ante consultas de la ciudadanía.</p>
 					</li>
-					<li>
-						<span class="item">b.</span><p>Comunicar a terceros información estadística elaborada a partir de los datos personales de sus usuarios, cuando de dichos datos no sea posible identificar individualmente a los titulares (datos disociados), de conformidad a la Ley.</p>
+					<li class="type-latin">
+						<p>Comunicar a terceros información estadística elaborada a partir de los datos personales de sus usuarios, cuando de dichos datos no sea posible identificar individualmente a los titulares (datos disociados), de conformidad a la Ley.</p>
 					</li>
 				</ul>
 				<p>Aparte de las autorizaciones establecidas anteriormente, MINSEGPRES mantendrá la debida confidencialidad de los datos personales y no los trasmitirá a terceros, salvo cuando se deban entregar en razón de un mandato legal o una orden emanada de los Tribunales de Justicia que así lo requiera.</p>
@@ -53,21 +53,21 @@
 				<p>En todo momento, el usuario podrá ejercer los derechos otorgados por la Ley Nº19.628 y sus modificaciones posteriores. En específico, podrá:</p>
 				<ul>
 					<li>
-						<span class="item">1.</span><p>Solicitar información respecto de los bancos de datos de que sea responsable el organismo, el fundamento jurídico de su existencia, su finalidad, tipos de datos almacenados y descripción del universo de personas que comprende.</p>
+						<p>Solicitar información respecto de los bancos de datos de que sea responsable el organismo, el fundamento jurídico de su existencia, su finalidad, tipos de datos almacenados y descripción del universo de personas que comprende.</p>
 					</li>
 					<li>
-						<span class="item">2.</span><p>Solicitar información sobre los datos relativos a su persona, su procedencia y destinatario, el propósito del almacenamiento y la individualización de las personas u organismos a los cuales sus datos son transmitidos regularmente.</p>
+						<p>Solicitar información sobre los datos relativos a su persona, su procedencia y destinatario, el propósito del almacenamiento y la individualización de las personas u organismos a los cuales sus datos son transmitidos regularmente.</p>
 					</li>
 					<li>
-						<span class="item">3.</span><p>Solicitar que se modifiquen sus datos personales cuando ellos no sean correctos o no estén actualizados, si fuere procedente.</p>
+						<p>Solicitar que se modifiquen sus datos personales cuando ellos no sean correctos o no estén actualizados, si fuere procedente.</p>
 					</li>
 					<li>
-						<span class="item">4.</span><p>Solicitar la eliminación o cancelación de los datos entregados cuando así lo desee, en tanto fuere procedente.</p>
+						<p>Solicitar la eliminación o cancelación de los datos entregados cuando así lo desee, en tanto fuere procedente.</p>
 					</li>
 				</ul>
-				<div class="altert-message">
+				<div class="important-message">
 					<div class="importante">IMPORTANTE</div>
-					Para ejercer sus derechos, el usuario podrá dirigirse a LA UNIDAD DE MODERNIZACIÓN Y GOBIERNO DIGITAL, ubicada en Teatinos Nº333, piso 4, Santiago (teléfono: +562 2 688-7701) o escribir al siguiente formulario de contacto, indicando claramente su solicitud.
+					<p>Para ejercer sus derechos, el usuario podrá dirigirse a LA UNIDAD DE MODERNIZACIÓN Y GOBIERNO DIGITAL, ubicada en Teatinos Nº333, piso 4, Santiago (teléfono: <a href="tel:+56226887701">+562 2 688-7701</a>) o escribir al siguiente formulario de contacto, indicando claramente su solicitud.</p>
 				</div>
 			</div>
 		</div>
