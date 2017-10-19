131439d85ca65ea34387c627645e775978a3e60e
Nicolas Silva <nsilvah@gmail.com>
2017-10-19 15:13:56 -0300

    fix #29

--- resources/views/layouts/backend.php
+++ resources/views/layouts/backend.php
@@ -85,4 +85,10 @@
 
     <?=view('chunks/footer')?>
 </div>
-<?=view('chunks/foot')?>
+<script>
+    window.baseUrl = "<?=url('')?>";
+</script>
+<script src="js/app.js"></script>
+
+</body>
+</html>
