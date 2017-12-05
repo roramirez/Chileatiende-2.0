36bf78a328915c7fd7f6e030954176de1c1fc890
unluck <diego@asimov.cl>
2017-12-05 19:00:56 -0300

    fix gráficos

--- resources/views/chunks/footer.php
+++ resources/views/chunks/footer.php
@@ -3,46 +3,67 @@
         <div class="footer-desktop">
             <div class="row">
                 <div class="col-sm-3">
-                    <h3>Sobre ChileAtiende</h3>
-                    <ul>
-                        <li><a href="/que-es-chileatiende">Sobre ChileAtiende</a></li>
-                        <li><a href="/instituciones">Instituciones que publican en el Portal</a></li>
-                        <li><a href="/ayuda/preguntas-frecuentes">Preguntas Frecuentes</a></li>
-                        <li><a href="/ayuda/">Centro de Ayuda</a></li>
-                    </ul>
+                    <a class="toggle-footer collapsed" data-toggle="collapse" href="#foo-coll-01">
+                        <h3>Sobre ChileAtiende</h3>
+                        <span class="caret"></span>
+                    </a>
+                    <div id="foo-coll-01" class="links-footer collapse">
+                        <ul>
+                            <li><a href="/que-es-chileatiende">Sobre ChileAtiende</a></li>
+                            <li><a href="/instituciones">Instituciones que publican en el Portal</a></li>
+                            <li><a href="/ayuda/preguntas-frecuentes">Preguntas Frecuentes</a></li>
+                            <li><a href="/ayuda/">Centro de Ayuda</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-sm-3">
-                    <h3>API para Desarrolladores</h3>
-                    <ul>
-                        <li><a href="/desarrolladores">API</a></li>
-                    </ul>
+                    <a class="toggle-footer collapsed" data-toggle="collapse" href="#foo-coll-02">
+                        <h3>API para Desarrolladores</h3>
+                        <span class="caret"></span>
+                    </a>
+                    <div id="foo-coll-02" class="links-footer collapse">
+                        <ul>
+                            <li><a href="/desarrolladores">API</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-sm-3">
-                    <h3>Accesos Directos</h3>
-                    <ul>
-                        <li><a href="?skin=mujer">ChileAtiende Mujer</a></li>
-                        <li><a href="?skin=exterior">ChileAtiende en el Exterior</a></li>
-                        <?php /*<li><a>ChileAtiende en tu sitio</a></li>*/ ?>
-                        <li><a href="/enlaces-transparencia">Enlaces para transparencia activa</a></li>
-                    </ul>
+                    <a class="toggle-footer collapsed" data-toggle="collapse" href="#foo-coll-03">
+                        <h3>Accesos Directos</h3>
+                        <span class="caret"></span>
+                    </a>
+                    <div id="foo-coll-03" class="links-footer collapse">
+                        <ul>
+                            <li><a href="?skin=mujer">ChileAtiende Mujer</a></li>
+                            <li><a href="?skin=exterior">ChileAtiende en el Exterior</a></li>
+                            <li><a href="/enlaces-transparencia">Enlaces para transparencia activa</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-sm-3">
-                    <h3>Términos y Condiciones</h3>
-                    <ul>
-                        <li><a href="/politica-de-privacidad">Política de privacidad</a></li>
-                        <li><a href="/terminos-y-condiciones">Términos de uso</a></li>
-                    </ul>
+                    <a class="toggle-footer collapsed" data-toggle="collapse" href="#foo-coll-04">
+                        <h3>Términos y Condiciones</h3>
+                        <span class="caret"></span>
+                    </a>
+                    <div id="foo-coll-01" class="links-footer collapse">
+                        <ul>
+                            <li><a href="/politica-de-privacidad">Política de privacidad</a></li>
+                            <li><a href="/terminos-y-condiciones">Términos de uso</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-6">
-                    <div><a href="tel:101"><img src="images/callcenter.svg" alt="callcenter" /></a> <a href="tel:101">Callcenter 101</a></div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="text-right">
-                        <a href="https://www.facebook.com/ChileAtiende" target="_blank"><img src="images/facebook.svg" alt="Facebook" /></a>
-                        &nbsp;
-                        <a href="https://twitter.com/ChileAtiende" target="_blank"><img src="images/twitter.svg" alt="Twitter" /></a>
+                <div class="contact-section">
+                    <div class="col-sm-6">
+                        <div><a href="tel:101"><img src="images/callcenter.svg" alt="callcenter" /></a> <a href="tel:101">Callcenter 101</a></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="text-right">
+                            <a href="https://www.facebook.com/ChileAtiende" target="_blank"><img src="images/facebook.svg" alt="Facebook" /></a>
+                            &nbsp;
+                            <a href="https://twitter.com/ChileAtiende" target="_blank"><img src="images/twitter.svg" alt="Twitter" /></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,8 +72,8 @@
             
         </div>
         <hr />
-        <div>ChileAtiende es una marca registrada por: <a href="http://www.ips.gob.cl" target="_blank">Instituto de Previsión Social (MINTRAB)</a></div>
-        <div>Portal desarrollado por: <a href="http://www.modernizacion.gob.cl" target="_blank">Unidad de Modernización y Gobierno Digital (MINSEGPRES)</a></div>
+        <div>ChileAtiende es una marca registrada por: <br><a href="http://www.ips.gob.cl" target="_blank">Instituto de Previsión Social (MINTRAB)</a></div>
+        <div>Portal desarrollado por: <br><a href="http://www.modernizacion.gob.cl" target="_blank">Unidad de Modernización y Gobierno Digital (MINSEGPRES)</a></div>
         <br />
         <div><a href="http://creativecommons.org/licenses/by/3.0/cl/" target="_blank"><img src="images/by.svg" /></a> <a href="http://creativecommons.org/licenses/by/3.0/cl/" target="_blank"><img src="images/cc.svg" /></a> A menos que se indique lo contrario, todo el contenido en ChileAtiende.gob.cl está bajo una licencia <a href="http://creativecommons.org/licenses/by/3.0/cl/" target="_blank">Creative Commons 3.0 (CC BY 3.0 CL)</a></div>
         <img class="gob" src="images/gob.svg" alt="Gobierno de Chile" />
