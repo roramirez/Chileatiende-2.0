28f510883ff66bd830aed7c8dfec9c659fb58c5e
Nicolas Silva <nsilvah@gmail.com>
2018-01-29 15:49:49 -0300

    Se quita codigo Olark

--- resources/views/chunks/foot.php
+++ resources/views/chunks/foot.php
@@ -26,20 +26,6 @@
 
 </script>
 
-<!-- begin olark code -->
-<script type="text/javascript" async>
-    ;(function(o,l,a,r,k,y){if(o.olark)return;
-        r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
-        y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
-        y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
-        y.extend=function(i,j){y("extend",i,j)};
-        y.identify=function(i){y("identify",k.i=i)};
-        y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
-        k=y._={s:[],t:[+new Date],c:{},l:a};
-    })(window,document,"static.olark.com/jsclient/loader.js");
-    /* Add configuration calls below this comment */
-    olark.identify('7728-462-10-9835');</script>
-<!-- end olark code -->
 <!-- Hotjar Tracking Code for http://chileatiende.asimov.cl/ --> <script> (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:622017,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); </script>
 <script src="//f1-na.readspeaker.com/script/6404/ReadSpeaker.js?pids=embhl" type="text/javascript"></script>
 <script>
