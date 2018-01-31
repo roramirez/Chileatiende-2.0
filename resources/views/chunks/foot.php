15287c8b47e60684942a998ef5526eda9f855553
Nicolas Silva <nsilvah@gmail.com>
2018-01-31 17:20:53 -0300

    Se cambia codigo de analytics

--- resources/views/chunks/foot.php
+++ resources/views/chunks/foot.php
@@ -1,29 +1,11 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-
-<!-- marca nueva -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-83320962-5"></script>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-28124406-2"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-83320962-5');
-</script>
 
-<!-- marca antigua -->
-<script type="text/javascript">
-
-    var _gaq = _gaq || [];
-    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-    _gaq.push(['_setAccount', 'UA-28124406-2']);
-    _gaq.push(['_setDomainName', 'chileatiende.gob.cl']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
+    gtag('config', 'UA-28124406-2');
 </script>
 
 <!-- Hotjar Tracking Code for http://chileatiende.asimov.cl/ --> <script> (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:622017,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); </script>
