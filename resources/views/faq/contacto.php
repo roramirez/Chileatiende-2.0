7ea9e04ec1106211a8cf820a771d1fc931f6353f
salvadorcea2 <36169518+salvadorcea2@users.noreply.github.com>
2018-05-31 15:59:32 -0400

    contacto iframe

---
+++ resources/views/faq/contacto.php
@@ -0,0 +1,32 @@
+<div id="faq">
+	<div class="container">
+		<ol class="breadcrumb">
+            <li><a href=""><i class="material-icons">home</i></a></li>
+            <li><a href="/ayuda/">Centro de Ayuda</a></li>
+            <li>Preguntas Frecuentes</li>
+        </ol>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="help-heading">
+					<div class="subheading">
+						Bienvenido al centro de ayuda
+					</div>
+					<h2>¿En qué te podemos ayudar?</h2>
+				</div>
+			</div>
+		</div>
+            
+            <div class="hidden-xs">
+		<?=view('chunks/help/help-cards')?>
+		 
+            </div>
+                        <div class="row" style="position: relative;height: 1000px; width:100%;overflow: hidden;margin-bottom: 1.5rem; ">
+                        <iframe frameborder="0"  style="position:absolute;top:0;left:0;width:100%;height:100%;margin:0!important;"
+                         src="https://www.chileatiende.gob.cl/contacto/formulario.php?origen=http://www.chileatiende.gob.cl/"> test</iframe>
+                    
+                    
+                        </div>
+	</div>
+</div>
