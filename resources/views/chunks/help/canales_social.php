02b58ec7b75b36d04b0be590a89b1b21365221cd
salvadorcea2 <36169518+salvadorcea2@users.noreply.github.com>
2018-05-31 16:01:13 -0400

    menu social

---
+++ resources/views/chunks/help/canales_social.php
@@ -0,0 +1,62 @@
+
+       <div class="hidden-md hidden-lg col-xs-3">
+             
+            <a href="https://es-la.facebook.com/ChileAtiende/" class="help-card" >
+                <div class="media">
+                    <div class="media-left">
+                        <i class="material-icons">thumb_up</i>
+                    </div>
+                    <div class="media-body">
+                        <div class="media-heading">Redes Sociales</div>
+                        <p>Conectate con nuestras redes sociales.</p>
+                    </div>
+                </div>
+            </a>
+           
+       </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 hidden-xs">
+             
+            <a href="https://es-la.facebook.com/ChileAtiende/" class="help-card dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <div class="media">
+                    <div class="media-left">
+                        <i class="material-icons">thumb_up</i>
+                    </div>
+                    <div class="media-body">
+                        <div class="media-heading">Redes Sociales</div>
+                        <p>Conectate con nuestras redes sociales.</p>
+                    </div>
+                </div>
+            </a>
+            
+            <div class="dropdown-menu help-menu hidden- ">
+                <div class="media">
+			<div class="media-left">
+				<img src="images/facebook-logo.svg" alt="Facebook logo">
+			</div>
+			<div class="media-body">
+				<div class="option">
+					<a href="https://www.facebook.com/ChileAtiende" target="_blank">Ir a facebook</a>
+				</div>
+				<div class="name">
+					/ChileAtiende
+				</div>
+			</div>
+		</div>
+		<div class="media">
+			<div class="media-left">
+				<img src="images/twitter-logo.svg" alt="twitter logo">
+			</div>
+			<div class="media-body">
+				<div class="option">
+					<a href="https://twitter.com/ChileAtiende" target="_blank">Ir a twitter</a>
+				</div>
+				<div class="name">
+					@ChileAtiende
+				</div>
+			</div>
+		</div>
+            </div>
+            
+            
+        </div>
